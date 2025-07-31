112dbff75a549af2da9e8b8956f201ce51fe5c72
julien-data-analyst-V2 <julienrenoult76460@gmail.com>
2025-07-31 11:15:33 +0200

    modified memory usage on the extract part

--- app.py
+++ app.py
@@ -12,6 +12,21 @@ from pathlib import Path
 import time
 #from memory_profiler import profile
 import pandas as pd
+import psutil
+import os
+import warnings
+#from memory_profiler import memory_usage
+
+# Enlever les avertissements de Pandas (notamment pour le read_csv)
+warnings.simplefilter(action='ignore', category=pd.errors.DtypeWarning)
+
+def get_memory_usage_mb():
+    """
+    Récupérer l'utilisation en mémoire du programme en question
+    """
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    return mem_info.rss / (1024 ** 2)  # Convertir en MB
 
 # Création de l'application
 app = Flask(__name__)
@@ -24,6 +39,8 @@ CACHE_DIR = Path("cache")
 CACHE_DIR.mkdir(exist_ok=True)
 CACHE_TAR_FILE = CACHE_DIR / "openradiation_dataset.tar.gz"
 CACHE_CSV_FILE = CACHE_DIR / "measurements.csv"
+# Pour les logs
+LOG_FILE = CACHE_DIR / "memoire.log"
 
 # Durée maximale de validité du cache (en secondes) = 24h
 MAX_CACHE_AGE = 24 * 60 * 60  # 86400 secondes
@@ -75,21 +92,35 @@ def ensure_csv_cached():
             # Extraire le fichier CSV et le lire pour en extraire les données / pour remplacer l'ancienne extraction
             with tar.extractfile(member) as f_in, open(CACHE_CSV_FILE, "wb") as f_out:
                 f_out.write(f_in.read())
-
+        
+        # Appel de la fonction
+        mem_used = get_memory_usage_mb()
+        print(f"Mémoire utilisée extraction dans le dossier compressé : {mem_used}")
         print("Fichier CSV mis à jour dans le cache")
 
-# Fonction de Streaming CSV (sans pandas)
-#@profile
-def generate():
-    with open(CACHE_CSV_FILE, "rb") as file:
-        # Lire l'en-tête séparément
-        header = pd.read_csv(file, sep=";", nrows=0).to_csv(index=False)
-        yield header
+# Permet d'enregistrer les différents étapes de mémoires utilisées
+def log_memory_usage(message: str):
+    """
+    Fonction : permet d'enregistrer un message dans le log en indiquant la date et l'heure de création qui va avec.
 
-        # Remettre à zéro pour tout relire
-        file.seek(0)
-        for chunk in pd.read_csv(file, sep=";", chunksize=100_000):
-            yield chunk.to_csv(index=False, header=False)
+    Arguments :
+    - message : le message à mettre dans le fichier
+
+    """
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        f.write(f"[{timestamp}] {message}\n")
+
+# Vérification de l'utilisation de la mémoire (limite 512 MB)
+def get_memory_usage_mb():
+    """
+    Fonction : permet de revoyer l'utilisation de mémoire de l'application
+    """
+    # permet de connaître le coût en mémoire du processus
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+
+    return mem_info.rss / (1024 ** 2)  # Convertir en MB
 
 
 ##########################################-
@@ -100,6 +131,30 @@ def generate():
 @app.route('/', methods=["GET", "POST"]) # Préciser les requêtes http possibles, sinon erreur 405 (si autre que GET)
 def index():
     return '<h1>Bienvenue sur le serveur Flask des données concernant OpenRadiation</h1>'
+
+@app.route("/memoire")
+def memoire_utilisee():
+    """
+    Fonction : Page sur les mémoires utilisées de l'application Flask.
+    """
+
+    # Récupérer la mémoire utilisée pour cette page
+    mem_used = get_memory_usage_mb()
+    log_memory_usage(f"Consultation mémoire actuelle : {mem_used:.2f} MB")
+
+    # Récupérer les différents logs concernant l'extraction
+    if LOG_FILE.exists():
+        logs = LOG_FILE.read_text(encoding="utf-8")
+    else:
+        logs = "Aucun log trouvé."
+
+    # Ajout dans un HTML
+    html = f"""
+        <h1>Utilisation mémoire actuelle : {mem_used:.2f} MB</h1>
+        <pre>{logs}</pre>
+    """
+
+    return html
 
 @app.route('/api/data/', methods=["GET"])
 def renitialiser_cache():
@@ -114,8 +169,9 @@ def renitialiser_cache():
         return "<h1> Suppression de la cache réussie </h1>"
     else:
         return "<h1> La cache n'existait pas avant </h1>"
-    
+
 # Api pour récupérer les mesures d'OpenRadiation
+#@profile
 @app.route('/api/data/measurements.csv')
 def streaming_csv_measurements():
     """
@@ -126,7 +182,17 @@ def streaming_csv_measurements():
     # Vérifie que le fichier est en cache, sinon le télécharge et l'extrait du fichier compressé
     ensure_csv_cached()
 
-    # Retourne la réponse HTTP
+    # Mémoire plus faible
+    def generate():
+        for i, chunk in enumerate(pd.read_csv(CACHE_CSV_FILE, sep=";", chunksize=100_000)):
+            # Retourne la réponse HTTP
+            mem_used = get_memory_usage_mb()
+            msg = f"Mémoire utilisée dans la partie extraction CSV : {mem_used} MB"
+            #print(msg)
+            log_memory_usage(msg) # On enregistre
+            yield chunk.to_csv(index=False, header=(i == 0))
+
+
     return Response(generate(), mimetype="text/csv")
 
 #################################-
